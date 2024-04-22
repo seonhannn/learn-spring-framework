0ec5d75efc7b2f9dfa63e21c8c2d612163511928
seonhannn <seon.hannn@gmail.com>
2024-04-22 17:45:37 +0900

    Initial commit

---
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunnner;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+
+public class AppGamingBasicJava {
+
+    public  static  void main(String [] args) {
+
+//        var game = new MarioGame();
+//        var game = new SuperContraGame();
+        var game = new PacmanGame();
+        var gameRunner = new GameRunnner(game);
+        gameRunner.run();
+
+    }
+}
